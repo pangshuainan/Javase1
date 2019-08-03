3b6d4b43421af46750dd7cdadb4e6bb24f43687e
pangshuainan <982162895@qq.com>
2019-08-03 11:00:39 +0800

    Create yinlaoshi.java

---
+++ untitled/src/yinlaoshi.java
@@ -0,0 +1,26 @@
+import java.util.Random;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class yinlaoshi{
+    private static yinlaoshi AiLi;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        AiLi.pringXing(5,10 );
+    }
+    public static void pringXing(int m,int n) {
+        for (int i = 1; i <=5 ; i++) {
+            if (i==1 || i==5){
+                System.out.println("**************");
+            }else{
+                System.out.println("*            *");
+            }
+            for (int j = 1; j <=5; j++) {
+
+            }
+
+        }
+                }
+            }
+        
